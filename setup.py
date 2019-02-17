5d78620119f517c7d56aa39c068ec48c596c1a6f
Benjamin A. Laken <benlaken@gmail.com>
2019-02-17 18:17:10 +0100

    update setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Interface to modify geospatial layers in the Resource Watch API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Vizzuality/LMIPy",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
