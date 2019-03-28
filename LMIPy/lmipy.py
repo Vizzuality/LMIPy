f62188c2795c05be44b167b83e24242767f70ddd
Benjamin A. Laken <benlaken@gmail.com>
2019-03-28 22:15:49 +0100

    created the Dataset object

--- LMIPy/lmipy.py
+++ LMIPy/lmipy.py
@@ -1,10 +1,17 @@
 import requests
+import random
 
 
 class LMI:
+    """
+    Main class to connect to multiple objects in the Vizzuality API.
+    """
     def __init__(self, token=None, server='https://api.resourcewatch.org'):
         self.token = token
         self.server = server
+
+    def __repr__(self):
+        return f"LMI class object"        
 
     def set_token(self, token):
         """Set the API token"""
@@ -53,12 +60,12 @@ class LMI:
         if 'layer' in object_type:
             response_list = self.get_layers(search=search, env=env, app_string=app_string, limit=limit)
         else:
-            response_list = self.get_datasets(env=env, app_string=app_string, limit=limit) 
+            response_list = self.get_datasets(search=search, env=env, app_string=app_string, limit=limit) 
         
         self.response_list = response_list
         return
     
-    def get_datasets(self, env, app_string, limit):
+    def get_datasets(self, search, env, app_string, limit):
         """Return all datasets and connected items within a limit and specified environment"""
         hash = random.getrandbits(16)
         url = (f'{self.server}/v1/dataset?app={app_string}&env={env}&'
@@ -86,8 +93,10 @@ class LMI:
     
     
     def filter_results(self, response_list, search, limit):
-        """Search by a list of strings to return a filtered list"""
+        """Search by a list of strings to return a filtered list of Dataset or Layer objects"""
         filtered_response = []
+        collection = []
+        print(f'searching for {search}')
         for item in response_list:
             in_description = False
             in_name = False
@@ -101,4 +110,52 @@ class LMI:
             if in_name or in_description:
                 if len(filtered_response) < limit:
                     filtered_response.append(item)
-        return filtered_response
+
+                if item.get('type') == 'dataset':
+                    collection.append(Dataset(id_hash = item.get('id'), attributes=item.get('attributes')))
+                if item.get('type') == 'layer':
+                    collection.append(Layer())
+        return collection
+    
+    
+class Dataset:
+    """ 
+    This is the main Dataset class. 
+      
+    Attributes: 
+        id_hash (int): An ID hash. 
+        attributes (dic): A dictionary holding the attributes of a dataset. 
+    """
+    def __init__(self,id_hash=None, attributes=None, server='https://api.resourcewatch.org'):
+        self.id = id_hash
+        self.server = server
+        if not attributes:
+            self.attributes = self.get_dataset()
+        else:
+            self.attributes = attributes
+    
+    def __repr__(self):
+        return f"Dataset {self.id}"
+    
+    def get_dataset(self):
+        """
+        Retrieve a dataset from a server by ID.
+        """
+        hash = random.getrandbits(16)
+        url = (f'{self.server}/v1/dataset/{self.id}?hash={hash}')
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r.json().get('data').get('attributes')
+        else:
+            raise ValueError(f'Unable to get dataset {self.id} from {r.url}')
+            
+            
+
+class Layer:
+    def __init__(self):
+        self.id = None
+        self.dataset = None
+        
+    def __repr__(self):
+        return f"Layer {self.id}"
+        
