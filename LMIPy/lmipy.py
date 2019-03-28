3dae6a940c6987d0f6927a09d8c41de7ee228232
Benjamin A. Laken <benlaken@gmail.com>
2019-03-28 23:56:59 +0100

    added visual notebook feature

--- LMIPy/lmipy.py
+++ LMIPy/lmipy.py
@@ -1,5 +1,6 @@
 import requests
 import random
+from IPython.display import display, HTML
 
 
 class LMI:
@@ -55,15 +56,12 @@ class LMI:
         if app:
             app_string = ",".join(app)
         else:
-            raise ValueError('Please specify an app to search.')
-        
+            raise ValueError('Please specify an app to search.')   
         if 'layer' in object_type:
             response_list = self.get_layers(search=search, env=env, app_string=app_string, limit=limit)
         else:
             response_list = self.get_datasets(search=search, env=env, app_string=app_string, limit=limit) 
-        
-        self.response_list = response_list
-        return
+        return response_list
     
     def get_datasets(self, search, env, app_string, limit):
         """Return all datasets and connected items within a limit and specified environment"""
@@ -71,7 +69,7 @@ class LMI:
         url = (f'{self.server}/v1/dataset?app={app_string}&env={env}&'
                f'includes=layer,vocabulary,metadata&page[size]=1000&hash={hash}')
         r = requests.get(url)
-        print(r.url)
+        #print(r.url)
         response_list = r.json().get('data', None)
         if len(response_list) < 1:
             raise ValueError('No items found')
@@ -84,7 +82,7 @@ class LMI:
         url = (f"{self.server}/v1/layer?app={app_string}&env={env}"
                f"&includes=vocabulary,metadata&page[size]=1000&hash={hash}")
         r = requests.get(url)
-        print(r.url)
+        #print(r.url)
         response_list = r.json().get('data', None)
         if len(response_list) < 1:
             raise ValueError('No items found')
@@ -96,7 +94,7 @@ class LMI:
         """Search by a list of strings to return a filtered list of Dataset or Layer objects"""
         filtered_response = []
         collection = []
-        print(f'searching for {search}')
+        #print(f'searching for {search}')
         for item in response_list:
             in_description = False
             in_name = False
@@ -133,9 +131,42 @@ class Dataset:
             self.attributes = self.get_dataset()
         else:
             self.attributes = attributes
+        self.url = f"{server}/v1/dataset/{id_hash}?hash={random.getrandbits(16)}"
     
     def __repr__(self):
+        return self.__str__()
+    
+    def __str__(self):
         return f"Dataset {self.id}"
+    
+    def _repr_html_(self):
+        """For notebook"""
+        if self.attributes.get('connectorUrl'):
+            table_statement = (f"Origin source: <a href={self.attributes.get('connectorUrl')}"
+                               " target='_blank'>"
+                               f"{self.attributes.get('tableName')}"
+                               "</a>"
+                              )
+        else:
+            table_statement = "Data source X"
+        
+        html = ("<div class='item_container' style='height: auto; overflow: hidden; border: 1px solid #80ceb9;"
+                "border-radius: 2px; background: #f2fffb; line-height: 1.21429em; padding: 10px;''>"
+                "<div class='item_left' style='width: 210px; float: left;''>"
+                "<a href='https://resourcewatch.org/' target='_blank'>"
+                "<img class='itemThumbnail' src='https://resourcewatch.org/static/images/logo-embed.png'>"
+                "</a></div><div class='item_right' style='float: none; width: auto; overflow: hidden;''>"
+                f"<a href={self.url} target='_blank'>"
+                f"<b>{self.attributes.get('name')}</b>"
+                "</a>"
+                f"<br> {table_statement} 🗺Dataset in {', '.join(self.attributes.get('application')).upper()}."
+                f"<br>Last Modified: {self.attributes.get('updatedAt')}"
+                f"<br>Connector: {self.attributes.get('connectorType').title()}"
+                f" | Published: {self.attributes.get('published')}"
+                " </div> </div>")
+        
+        return html
+        
     
     def get_dataset(self):
         """
@@ -148,8 +179,7 @@ class Dataset:
             return r.json().get('data').get('attributes')
         else:
             raise ValueError(f'Unable to get dataset {self.id} from {r.url}')
-            
-            
+
 
 class Layer:
     def __init__(self):
