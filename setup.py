e0f1b543dbcb8cdbb593023f3da22b0b50a7ddeb
Benjamin A. Laken <benlaken@gmail.com>
2019-04-05 12:54:01 +0200

    updating config files

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="LMIPy",
-    version="0.0.5",
+    version="0.0.6",
     author="Vizzuality",
-    author_email="benlaken@icloud.com",
+    author_email="benjamin.laken@vizzuality.com",
     description="Interface to data and layers in the Resource Watch API",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='MIT',
     url="https://github.com/Vizzuality/LMIPy",
-    install_requires=['requests>=2.2.0'],
+    install_requires=['requests>=2.2.0','folium','geopandas','cartoframes'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
