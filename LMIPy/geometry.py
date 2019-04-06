71832f8a9b32287e2eec5c42a409b53836da2dac
Benjamin A. Laken <benlaken@gmail.com>
2019-04-06 15:51:23 +0200

    added ability to create geometry from geojson directly

--- LMIPy/geometry.py
+++ LMIPy/geometry.py
@@ -10,26 +10,21 @@ from .utils import html_box
 
 class Geometry:
     """
-    This is the main Layer class.
+    This is the main geometry class.
 
     Parameters
     ----------
     id_hash: int
-        An ID hash.
+        An ID hash of a Geostore from a Vizzuality endpoint.
     attributes: dic
-        A dictionary holding the attributes of a dataset.
+        A valid geojson representation.
     server: str
-        A string of the server URL.
+        The string of the server URL.
     """
-    def __init__(self, id_hash=None, attributes=None, server='http://production-api.globalforestwatch.org'):
+    def __init__(self, id_hash=None, attributes=None, server='https://production-api.globalforestwatch.org'):
         self.server = server
-        if not id_hash:
-            if attributes:
-                self.id = attributes.get('id', None)
-                self.attributes = attributes.get('attributes', None)
-            else:
-                self.id = None
-                self.attributes = None
+        if attributes:
+            self.attributes = self.create_geostore_from_geojson(attributes)
         else:
             self.id = id_hash
             self.attributes = self.get_geometry()
@@ -43,12 +38,32 @@ class Geometry:
     def _repr_html_(self):
         return html_box(item=self)
 
-    def get_geometry(self, simplify=False):
+    def create_geostore_from_geojson(self, attributes):
+        """Parse valid geojson into a geostore object and register it to a
+        Gestore object on a server. Return the object, and instantiate a
+        Geometry.
         """
-        Returns a layer from a Vizzuality API.
+        try:
+            body = json.loads(json.dumps(attributes))
+        except:
+            raise ValueError(f"Unable to pass attributes. Expected valid geojson, recieved: {attrributes}")
+        header= {
+                'Content-Type':'application/json'
+                }
+        url = self.server + '/v1/geostore'
+        r = requests.post(url, headers=header, json=body)
+        if r.status_code == 200:
+            self.id = r.json().get('data').get('id')
+            return r.json().get('data').get('attributes')
+        else:
+            raise ValueError(f'Recieved response of {r.status_code} from {r.url} when posting to geostore.')
+
+    def get_geometry(self, simplify=False, version='v2'):
+        """
+        Returns a geostore object by ID from a Vizzuality endpoint.
         """
         hash = random.getrandbits(16)
-        url = (f'{self.server}/v2/geostore/{self.id}?simplify={simplify}&hash={hash}')
+        url = (f'{self.server}/{version}/geostore/{self.id}?simplify={simplify}&hash={hash}')
         r = requests.get(url)
         if r.status_code == 200:
             return r.json().get('data').get('attributes')
@@ -66,13 +81,11 @@ class Geometry:
             'bbox': attributes['bbox'],
             'id': attributes['hash']
          }
-        
         features = attributes['geojson']['features']
         if len(features) > 0:
             gdf = gpd.GeoDataFrame([{**props, 'geometry': shape(feature['geometry'])} for feature in features]).set_geometry('geometry')
             gdf.crs = {'init' :'epsg:4326'}
             return gdf
-
         return []
 
     def shape(self):
@@ -95,27 +108,21 @@ class Geometry:
             'bbox',
             'id'
         ]
-
         bbox = self.attributes['bbox']
         shapely_geometry = shape(geometry)
         centroid = list(shapely_geometry.centroid.coords)[0][::-1]
         bounds = [bbox[2:][::-1], bbox[:2][::-1]]
-
         map = folium.Map(
             location=centroid,
             tiles='Mapbox Bright',
         )
-
         if geometry['type'] == 'Point':
             folium.Marker(
                 centroid
             ).add_to(map)
-
         else:
             folium.GeoJson(
                 data=self.table()
                 ).add_to(map)
-                
             map.fit_bounds(bounds)
-
         return map
