ca8691d64e38301f631740223de2886b0ad88503
Benjamin A. Laken <benlaken@gmail.com>
2019-04-10 13:15:09 +0200

    tweaks

--- LMIPy/geometry.py
+++ LMIPy/geometry.py
@@ -25,14 +25,14 @@ class Geometry:
     s: obj
         A shapely object.
     """
-    def __init__(self, id_hash=None, attributes=None, s=None, params={}, server='https://production-api.globalforestwatch.org'):
+    def __init__(self, id_hash=None, attributes=None, s=None, parameters={}, server='https://production-api.globalforestwatch.org'):
         self.server = server
         if s:
             attributes = self.create_attributes_from_shapely(s)
         if attributes:
             self.attributes = self.create_geostore_from_geojson(attributes)
-        if params:
-            self.attributes = self.create_attributes_from_table(params)
+        if parameters:
+            self.attributes = self.create_attributes_from_table(parameters)
         else:
             self.id = id_hash
             self.attributes = self.get_geometry()
@@ -58,29 +58,24 @@ class Geometry:
         else:
             raise ValueError('shape object was not of suitable geometry type')
 
-    def create_attributes_from_table(self, params={}):
-        if not params:
-            raise ValueError(f'Params requires!')
-        
-        iso = params.get('iso', None)
-        id1 = params.get('adm1', None)
-        id2 = params.get('adm2', None)
-        gadm = params.get('gadm', None)
-        cartodb_id = params.get('id', None)
-        table = params.get('table', None)
-
+    def create_attributes_from_table(self, parameters={}):
+        if not parameters:
+            raise ValueError(f'parameters requires!')
+        iso = parameters.get('iso', None)
+        id1 = parameters.get('adm1', None)
+        id2 = parameters.get('adm2', None)
+        gadm = parameters.get('gadm', None)
+        cartodb_id = parameters.get('id', None)
+        table = parameters.get('table', None)
         gadm_ver = {
             '2.7': 'v1',
             '3.6': 'v2'
         }
-
         if not gadm:
             gadm = '3.6'
         elif gadm not in ['2.7', '3.6']:
             raise ValueError(f'GADM must be 2.7 (v1) or 3.6 (v2)')
-
         version = gadm_ver[gadm]
-
         if iso:
             if id2 and id1 and iso:
                 url = f'/{version}/geostore/admin/{iso}/{id1}/{id2}'
@@ -89,16 +84,14 @@ class Geometry:
             elif iso:
                 url = f'/{version}/geostore/admin/{iso}'
             else:
-                raise ValueError(f'Invalid admin params. Requires: iso, adm1, adm2 keys')
+                raise ValueError(f'Invalid admin parameters. Requires: iso, adm1, adm2 keys')
         elif table:
             if table and cartodb_id:
                 url = f'/{version}/geostore/use/{table}/{int(cartodb_id)}'
             else:
-                raise ValueError(f'Invalid table params. Requires: table and cartodb_id keys')
-
+                raise ValueError(f'Invalid table parameters. Requires: table and cartodb_id keys')
         else:
-            raise ValueError(f'Invalid params! Valid keys: [iso, adm1, adm2] or [table, cartodb_id]')
-
+            raise ValueError(f'Invalid parameters! Valid keys: [iso, adm1, adm2] or [table, cartodb_id]')
         header= {
                 'Content-Type':'application/json'
                 }
@@ -174,12 +167,6 @@ class Geometry:
         """
         geojson = self.attributes['geojson']
         geometry = geojson['features'][0]['geometry']
-        fields = [
-            *list(self.attributes['info'].keys()),
-            'areaHa',
-            'bbox',
-            'id'
-        ]
         bbox = self.attributes['bbox']
         shapely_geometry = shape(geometry)
         centroid = list(shapely_geometry.centroid.coords)[0][::-1]
@@ -192,13 +179,11 @@ class Geometry:
             folium.Marker(
                 centroid
             ).add_to(map)
-
         else:
             folium.GeoJson(
                 data=self.table()
                 ).add_to(map)
             map.fit_bounds(bounds)
-
         if image:
             url = f"https://production-api.globalforestwatch.org/v1/recent-tiles?lat={centroid[1]}&lon={centroid[0]}&start=2019-01-01&end=2019-04-01"
             r = requests.get(url)
@@ -210,5 +195,4 @@ class Geometry:
                     tiles=tile_url,
                     attr="Live EE tiles"
                 )
-
         return map
