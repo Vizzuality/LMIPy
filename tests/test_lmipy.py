2b8df1c3b833f0c33ad853aa611a6066882f42e4
Benjamin A. Laken <benlaken@gmail.com>
2019-04-13 11:16:22 +0200

    added test tweaks

--- tests/test_lmipy.py
+++ tests/test_lmipy.py
@@ -6,8 +6,6 @@ def test_create_dataset():
     assert ds.id == 'bb1dced4-3ae8-4908-9f36-6514ae69713f'
     assert type(ds.attributes) == dict
     assert len(ds.attributes) > 0
-    assert len(ds.vocabulary.attributes) > 0
-    assert len(ds.metadata.attributes) > 0
 
 def test_queries_on_datasets():
     ds = Dataset(id_hash='bd5d7924-611e-4302-9185-8054acb0b44b')
