3799d74733aec18c3d5ce45e807be832f31fa871
Adam J. Pain <aj.pain89@gmail.com>
2019-07-05 17:59:34 +0200

    Add more meta/vocab tests

--- tests/test_lmipy.py
+++ tests/test_lmipy.py
@@ -1,4 +1,5 @@
 import pytest
+import random
 from LMIPy import Dataset, Collection, Layer, Metadata, Vocabulary, Widget, Image, ImageCollection, Geometry
 
 ### Collection Tests
@@ -53,6 +54,15 @@ def test_access_widget():
 
 ### Clone Dataset
 ### Update Dataset
+def test_update_dataset():
+    hash = random.getrandbits(8)
+    ds = Dataset(id_hash='bc06c603-9b16-4e51-99e6-228fa576e06b')
+    updated = ds.update(token=token, update_params={'name': f'Template Dataset #{hash}'})
+    assert type(updated.attributes['name']) == f'Template Dataset #{hash}'
+    updated = ds.update(token=token, update_params={'name': f'Template Dataset #{hash}'})
+    assert type(updated.attributes['name']) == 'Template Dataset'
+
+
 ### Delete Dataset
 
 #----- Layer Tests -----#
@@ -60,6 +70,18 @@ def test_access_widget():
 def test_layer_creation():
     ly = Layer(id_hash='dc6f6dd2-0718-4e41-81d2-109866bb9edd')
     assert ly is not None
+
+### Clone Layer
+### Update Layer
+def test_update_layer():
+    hash = random.getrandbits(8)
+    l = Layer(id_hash='0328715e-6c6e-4e11-8177-5f0681794f8d')
+    updated = l.update(token=token, update_params={'name': f'Template Layer #{hash}'})
+    assert type(updated.attributes['name']) == f'Template Layer #{hash}'
+    updated = l.update(token=token, update_params={'name': f'Template Layer #{hash}'})
+    assert type(updated.attributes['name']) == 'Template Layer'
+
+### Delete Layer
 
 ### Widget Tests
 
@@ -69,21 +91,86 @@ def test_create_widget():
     assert type(w.attributes) == dict
     assert len(w.attributes) > 0
 
-### Clone Layer
+### Add Widget
 ### Update Layer
 ### Delete Layer
 
+
 #----- Vocab Tests -----#
 
-### Add Vocab
-### Update Vocab
 ### Delete Vocab
+def test_delete_vocab():
+    ds = Dataset(id_hash='bc06c603-9b16-4e51-99e6-228fa576e06b')
+    v = ds.vocabulary[0].id
+    assert type(v) == str
+    deleted_vocab = v.delete(token=token)
+    assert deleted_vocab == None
+
+### Add Vocab
+def test_add_vocab():
+    ds = Dataset(id_hash='bc06c603-9b16-4e51-99e6-228fa576e06b')
+    payload = {
+        'name': 'categoryTab',
+        'tags': ['forestChange', 'treeCoverChange'],
+        'application': 'gfw'
+    }
+    updated_ds = ds.add_metadata(vocab_params=payload, token=token)
+    assert len(updated_ds.vocabulary) > 0
+
+### Update Vocab
+def test_update_vocab():
+    ds = Dataset(id_hash='bc06c603-9b16-4e51-99e6-228fa576e06b')
+    v = ds.vocabulary[0].id
+    assert type(v) == str
+    payload = {
+        'name': 'categoryTab',
+        'tags': ['forestChange', 'treeCoverChange']
+    }
+    updated_v = v.update(update_params=payload, token=token)
+    assert updated_v[0].attributes['name'] == 'categoryTab'
+    assert updated_v[0].attributes['tags'] == ['forestChange', 'treeCoverChange']
 
 #----- Meta Tests -----#
 
-### Clone Meta
-### Update Meta
 ### Delete Meta
+def test_delete_meta():
+    ds = Dataset(id_hash='bc06c603-9b16-4e51-99e6-228fa576e06b')
+    m = ds.metadata[0].id
+    assert type(m) == str
+    deleted_meta = m.delete(token=token)
+    assert deleted_meta == None
+
+### Add Meta
+def test_add_meta():
+    ds = Dataset(id_hash='bc06c603-9b16-4e51-99e6-228fa576e06b')
+    payload = {
+    'application': 'gfw',
+    'info': {'citation': 'Example citation',
+        'color': '#fe6598',
+        'description': 'This is an example dataset.',
+        'isLossLayer': True,
+        'name': 'Template Layer'},
+        'language': 'en'
+    }
+    updated_ds = ds.add_metadata(meta_params=payload, token=token)
+    assert len(updated_ds.metadata) > 0
+
+### Update Meta
+def test_update_meta():
+    ds = Dataset(id_hash='bc06c603-9b16-4e51-99e6-228fa576e06b')
+    m = ds.metadata[0].id
+    assert type(m) == str
+    payload = {
+    'application': 'gfw',
+    'info': {'citation': 'TEST',
+        'color': '#fe6598',
+        'description': 'TEST',
+        'isLossLayer': False,
+        'name': 'Template Layer'},
+    'language': 'en'}
+    updated_m = m.update(update_params=payload, token=token)
+    assert updated_m[0].attributes['info']['description'] == 'TEST'
+    assert updated_m[0].attributes['info']['isLossLayer'] == False
 
 #----- Geometry Tests -----#
 
