8d0683da8a5587538cf2b73ba775a8ed15d3f088
Adam J. Pain <aj.pain89@gmail.com>
2019-09-24 11:04:55 +0200

    Add merge widget

--- LMIPy/dataset.py
+++ LMIPy/dataset.py
@@ -570,14 +570,14 @@ class Dataset:
             
     def merge(self, token=None, target_dataset=None, target_dataset_id=None, target_server='https://api.resourcewatch.org', key_whitelist=[], force=False):
         """
-        'Merge' one Dataset entity into another target Layer.
+        'Merge' one Dataset entity into another target Dataset.
         The argument `key_whitelist` can be used to specify which properties you wish to merge (if not all)
         Note: requires API token.
         """
         if not token:
             raise ValueError(f'[token] Resource Watch API token required to update Dataset.')
         if not target_dataset and target_dataset_id and target_server:
-            target_dataset = Layer(target_dataset_id, server=target_server)
+            target_dataset = Dataset(target_dataset_id, server=target_server)
         else:
             raise ValueError(f'Requires either target Dataset or Dataset id plus server.')
         atts = self.attributes
@@ -592,7 +592,7 @@ class Dataset:
         }
         if not key_whitelist: key_whitelist = [k for k in payload.keys()]
         filtered_payload = {k:v for k,v in payload.items() if v and k in key_whitelist}
-        print(f'Merging {self.id} from {self.server} into {target_data_id} on {target_server}.\nAre you sure you sure you want to continue?')
+        print(f'Merging {self.id} from {self.server} into {target_dataset_id} on {target_server}.\nAre you sure you sure you want to continue?')
         if not force:
             conf = input()
         else:
