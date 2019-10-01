581f00058c03fb7824ce845ac65ee462443ff962
Adam J. Pain <aj.pain89@gmail.com>
2019-10-01 16:56:00 +0200

    Update widget  tests

--- tests/widget_tests.py
+++ tests/widget_tests.py
@@ -1,4 +1,9 @@
+import copy
+import json
 import os.path
+import re
+
+import requests_mock
 
 from LMIPy import Dataset, Widget
 
@@ -9,7 +14,6 @@ except:
 
 
 ### Widget Tests
-
 def test_create_widget():
     w = Widget(id_hash='8571b2c4-9478-4b63-8444-d308b191df92')
     assert w.id == '8571b2c4-9478-4b63-8444-d308b191df92'
@@ -18,8 +22,31 @@ def test_create_widget():
 
 
 ### Delete Widget
+@requests_mock.mock(kw='mock')
+def test_delete_widget(**kwargs):
+    dataset_matcher = re.compile(
+        'https://api.resourcewatch.org/v1/dataset/7cf3fab2-3fbe-4980-b572-712207b2c8c7\?includes=layer,widget,vocabulary,metadata&hash=(\w*)'
+    )
 
-def test_delete_widget():
+    working_directory = os.getcwd()
+    with open(working_directory + "/tests/test_assets/datasets/GET_7cf3fab2-3fbe-4980-b572-712207b2c8c7.json") as json_file:
+        dataset = json.load(json_file)
+
+    kwargs['mock'].get(dataset_matcher,
+                       [
+                           {
+                               'status_code': 200,
+                               'json': dataset
+                           }
+                       ])
+
+    kwargs['mock'].delete('https://api.resourcewatch.org/dataset/7cf3fab2-3fbe-4980-b572-712207b2c8c7/widget/e1436fc4-6118-4097-b022-981ca94bbd3b',
+                       [
+                           {
+                               'status_code': 200,
+                               'json': {}
+                           }
+                       ])
     ds = Dataset(id_hash='7cf3fab2-3fbe-4980-b572-712207b2c8c7')
     w = ds.widget[0]
     assert type(w.id) == str
@@ -28,8 +55,47 @@ def test_delete_widget():
 
 
 ### Add Widget
+@requests_mock.mock(kw='mock')
+def test_add_widget(**kwargs):
+    dataset_matcher = re.compile(
+        'https://api.resourcewatch.org/v1/dataset/7cf3fab2-3fbe-4980-b572-712207b2c8c7\?includes=layer,widget,vocabulary,metadata&hash=(\w*)'
+    )
 
-def test_add_widget():
+    working_directory = os.getcwd()
+    with open(working_directory + "/tests/test_assets/datasets/GET_7cf3fab2-3fbe-4980-b572-712207b2c8c7.json") as json_file:
+        dataset = json.load(json_file)
+        updated_dataset = copy.deepcopy(dataset)
+    
+    widget = updated_dataset['data']['attributes']['widget'][0]
+    dataset['data']['attributes']['widget'] = []
+
+    widget['attributes'] = {
+        'name': 'Template Widget',
+        'widgetConfig': {'key': 'val'},
+        'application': ['gfw']
+    }
+
+    updated_dataset['data']['attributes']['widget'][0] = widget
+
+    kwargs['mock'].get(dataset_matcher,
+                       [
+                           {
+                               'status_code': 200,
+                               'json': dataset
+                           },
+                           {
+                               'status_code': 200,
+                               'json': updated_dataset
+                           }
+                       ])
+    
+    kwargs['mock'].post('https://api.resourcewatch.org/v1/dataset/7cf3fab2-3fbe-4980-b572-712207b2c8c7/widget',
+                       [
+                           {
+                               'status_code': 200,
+                               'json': {}
+                           }
+                       ])
     d = Dataset(id_hash='7cf3fab2-3fbe-4980-b572-712207b2c8c7')
     w = d.add_widget(widget_params={
         'name': 'Template Widget',
@@ -42,8 +108,58 @@ def test_add_widget():
 
 
 ### Update Widget
+@requests_mock.mock(kw='mock')
+def test_update_widget(**kwargs):
+    dataset_matcher = re.compile(
+        'https://api.resourcewatch.org/v1/dataset/7cf3fab2-3fbe-4980-b572-712207b2c8c7\?includes=layer,widget,vocabulary,metadata&hash=(\w*)'
+    )
+    widget_matcher = re.compile(
+        'https://api.resourcewatch.org/v1/widget/e1436fc4-6118-4097-b022-981ca94bbd3b&hash=(\w*)'
+    )
 
-def test_update_widget():
+    working_directory = os.getcwd()
+    with open(working_directory + "/tests/test_assets/datasets/GET_7cf3fab2-3fbe-4980-b572-712207b2c8c7.json") as json_file:
+        dataset = json.load(json_file)
+        updated_dataset = copy.deepcopy(dataset)
+    
+    widget = updated_dataset['data']['attributes']['widget'][0]
+    widget['attributes'] = {
+        'name': 'Widget UPDATED',
+        'widgetConfig': {'updated': True}
+    }
+    
+    updated_dataset['data']['attributes']['widget'][0] = widget
+
+    kwargs['mock'].get(dataset_matcher,
+                       [
+                           {
+                               'status_code': 200,
+                               'json': dataset
+                           },
+                           {
+                               'status_code': 200,
+                               'json': updated_dataset
+                           }
+                       ])
+    
+    kwargs['mock'].patch('https://api.resourcewatch.org/v1/dataset/7cf3fab2-3fbe-4980-b572-712207b2c8c7/widget/e1436fc4-6118-4097-b022-981ca94bbd3b',
+                       [
+                           {
+                               'status_code': 200,
+                               'json': {
+                                   'data': widget
+                               }
+                           }
+                       ])
+
+    kwargs['mock'].get(widget_matcher,
+                       [
+                           {
+                               'status_code': 200,
+                               'json': widget
+                           }
+                       ])            
+
     ds = Dataset(id_hash='7cf3fab2-3fbe-4980-b572-712207b2c8c7')
     w = ds.widget[0]
     assert type(w.id) == str
@@ -57,15 +173,70 @@ def test_update_widget():
 
 
 ### Merge Widget
+@requests_mock.mock(kw='mock')
+def test_merge_widget(**kwargs):
+    widget_matcher = re.compile(
+        'https://api.resourcewatch.org/v1/widget/25dcb710-6b85-4bfa-b09b-e4c70c33f381\?hash=(\w*)'
+    )
+    staging_widget_matcher = re.compile(
+        'https://staging-api.globalforestwatch.org/v1/widget/66de77eb-dee3-4c56-9ad4-cf68d8b107fd\?hash=(\w*)'
+    )
 
-def test_merge_widget():
+    working_directory = os.getcwd()
+    with open(working_directory + "/tests/test_assets/datasets/GET_7cf3fab2-3fbe-4980-b572-712207b2c8c7.json") as json_file:
+        widget = json.load(json_file)['data']['attributes']['widget'][0]
+        merged_widget = copy.deepcopy(widget)
+        merged_widget['id'] = "626e08ed-15b5-499a-8a46-9a5cb52d0a30"
+        merged_widget['attributes']['name'] = 'Template Widget Staging'
+        merged_widget['attributes']['widgetConfig'] = {}
+        merged_widget['attributes']['dataset'] = '66de77eb-dee3-4c56-9ad4-cf68d8b107fd'
+
+    whitelist = [
+        'widgetConfig',
+        'legendConfig',
+        'applicationConfig',
+        'interactionConfig',
+        'description',
+        'iso',
+        'application',
+        'provider',
+        'published'
+    ]
+
+    kwargs['mock'].get(widget_matcher,
+                       [
+                           {
+                               'status_code': 200,
+                               'json': {
+                                   'data': widget
+                               }
+                           }
+                       ])
+    
+    kwargs['mock'].get(staging_widget_matcher,
+                       [
+                           {
+                               'status_code': 200,
+                               'json': {
+                                   'data': merged_widget
+                               }
+                            }
+                       ])
+
+    kwargs['mock'].patch('https://staging-api.globalforestwatch.org/v1/dataset/66de77eb-dee3-4c56-9ad4-cf68d8b107fd/widget/66de77eb-dee3-4c56-9ad4-cf68d8b107fd',
+                         [
+                             {
+                                 'status_code': 200,
+                                 'json': {
+                                     'data': widget
+                                 }
+                             }
+                         ]
+                         )
+
     staging_widget = Widget(id_hash='66de77eb-dee3-4c56-9ad4-cf68d8b107fd',
                             server='https://staging-api.globalforestwatch.org')
-    staging_widget.update(update_params={
-        'name': 'Template Widget Staging',
-        'widgetConfig': {}
-    }, token=API_TOKEN)
-    production_widget = Dataset(id_hash='7cf3fab2-3fbe-4980-b572-712207b2c8c7').widget[0]
+    production_widget = Widget(id_hash='25dcb710-6b85-4bfa-b09b-e4c70c33f381')
     whitelist = ['name', 'widgetConfig']
     merged_widget = production_widget.merge(token=API_TOKEN,
                                             target_widget=None,
