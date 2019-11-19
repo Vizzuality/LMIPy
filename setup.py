ad5e83bc3035a1936d0fdbc5199beb948cb8d159
Benjamin A. Laken <benlaken@gmail.com>
2019-11-19 17:34:12 +0100

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="LMIPy",
-    version="0.3.17",
+    version="0.3.18",
     author="Vizzuality",
     author_email="benjamin.laken@vizzuality.com",
     description="Pythonic interface to various backend ecosystems related geospatial data.",
